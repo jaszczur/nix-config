c9a2357dcc38058302275e4e398ffcc6da28421c
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2017-10-06 13:00:30 +0000

    split to multiple packages

--- user/config.nix
+++ user/config.nix
@@ -5,9 +5,25 @@ with (import <nixpkgs> {});
   services.emacs.enable = true;
 
   packageOverrides = pkgs: with pkgs; {
+    jaszczurUtilities = pkgs.buildEnv {
+      name = "jaszczur-utilities";
+      paths = [ git exa ];
+    };
+    
+    jaszczurDevTools = pkgs.buildEnv {
+      name = "jaszczur-dev-tools";
+      paths = [ jetbrains.idea-community ];
+    };
+
+    jaszczurDesktop = pkgs.buildEnv {
+      name = "jaszczur-desktop";
+      paths = [ firefox-nightly-bin ];
+    };
+
+
     jaszczurPackages = pkgs.buildEnv {
       name = "jaszczur-packages";
-      paths = [ firefox-nightly-bin git jetbrains.idea-community ];
+      paths = [ jaszczurUtilities jaszczurDevTools jaszczurDesktop ];
     };
   };
 }
